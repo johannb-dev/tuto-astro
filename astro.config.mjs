a2177f8d08b01a7367ce0c4a62e6fd3ff71efd67
jbernard <johann.bernard@manche.fr>
2024-01-23 17:19:43 +0100

    Add RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://master--stirring-hummingbird-0a33b4.netlify.app/",
+});
